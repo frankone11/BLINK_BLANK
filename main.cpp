bf83442e7c368390f468b3848e3948cd9d9e83cc
Javo Garcia <frankone11@hotmail.com>
2023-12-20 22:00:16 -0600

    First commit with main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+
+#define BLINK_LED_ON  25ms
+#define BLINK_LED_OFF  975ms
+#define LED PC_13
+
+int main()
+{
+    DigitalOut led(LED);
+    BufferedSerial Serial(PA_2, PA_3);
+    Serial.set_baud(115200);
+
+    const char *texto = "Hola mundo\n";
+
+    while(true)
+    {
+        led = 1;
+        ThisThread::sleep_for(BLINK_LED_OFF);
+        led = 0;
+        Serial.write(texto, strlen(texto));
+        ThisThread::sleep_for(BLINK_LED_ON);
+    }
+}
